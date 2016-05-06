cc712cb3f5cb473b71b61d43c45829758420ba9f
armikhalev <armikhalev@gmail.com>
2016-05-06 17:27:21 -0400

    Fixes issues for handheld devices

--- js/index.js
+++ js/index.js
@@ -1,81 +1,86 @@
+// (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))isMobile=true})(navigator.userAgent||navigator.vendor||window.opera,'http://detectmobilebrowser.com/mobile');
+//
+// if (isMobile === true){
+//   alert("fok! Mobile!");
+// }
+// else {
+  $(document).ready(function() {
+    // cache the window object
+    $window = $(window);
 
-$(document).ready(function() {
-  // cache the window object
-  $window = $(window);
+    // hide arrow up on load
+    $('#arrow-up').fadeOut();
 
-  // smooth floating up welcome message
-  // $("#wlcm").addClass("up");
-  // hide arrow up on load
-  $('#arrow-up').fadeOut();
+    // Parallax effect
+    $('section[data-type="background"]').each(function() {
+      // declare the variable to affect the defined data-type
+      var $scroll = $(this);
 
-  // Parallax effect
-  $('section[data-type="background"]').each(function() {
-    // declare the variable to affect the defined data-type
-    var $scroll = $(this);
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards
+        var yPos = -($window.scrollTop() / $scroll.data('speed'));
 
+        // background position
+        var coords = '50% ' + yPos + 'px';
+        // move the background
+        $scroll.css({
+          backgroundPosition: coords
+        });
+      }); // end window scroll
+    }); // end section function
+
+    // shrinks navbar
     $(window).scroll(function() {
-      // HTML5 proves useful for helping with creating JS functions!
-      // also, negative value because we're scrolling upwards
-      var yPos = -($window.scrollTop() / $scroll.data('speed'));
+      if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+        $('#arrow-up').fadeIn();
+      } else {
+        $('nav').removeClass('shrink');
+        $('#arrow-up').fadeOut();
+      }
+    });
 
-      // background position
-      var coords = '50% ' + yPos + 'px';
-      // move the background
-      $scroll.css({
-        backgroundPosition: coords
-      });
-    }); // end window scroll
-  }); // end section function
+        // arrow-up changing color
+        $(window).scroll(function(){
 
-  // shrinks navbar
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').addClass('shrink');
-      $('#arrow-up').fadeIn();
-    } else {
-      $('nav').removeClass('shrink');
-      $('#arrow-up').fadeOut();
-    }
-  });
+            if($(this).scrollTop() >= $('.footer').position().top - 650) {
+                $("#arrow-up").css("background","#fff");
+            } else if($(this).scrollTop() >= $('.grey-blue-line').position().top + 250){
+                $("#arrow-up").css("background","none");
+            } else if($(this).scrollTop() >= $('.grey-blue-line').position().top - 650){
+                $("#arrow-up").css("background","#fff");
 
-      // arrow-up changing color
-      $(window).scroll(function(){
+            // change arrow up on .grey-line
+            }else if($(this).scrollTop() >= $('.grey-line').position().top - 450){
+                $("#arrow-up").css("background","none");
+            } else if($(this).scrollTop() >= $('.grey-line').position().top - 640){
+                $("#arrow-up").css("background","#fff");
 
-          if($(this).scrollTop() >= $('.footer').position().top - 650) {
-              $("#arrow-up").css("background","#fff");
-          } else if($(this).scrollTop() >= $('.grey-blue-line').position().top + 250){
+            }else{
               $("#arrow-up").css("background","none");
-          } else if($(this).scrollTop() >= $('.grey-blue-line').position().top - 650){
-              $("#arrow-up").css("background","#fff");
+            }
+        });
 
-          // change arrow up on .grey-line
-          }else if($(this).scrollTop() >= $('.grey-line').position().top - 450){
-              $("#arrow-up").css("background","none");
-          } else if($(this).scrollTop() >= $('.grey-line').position().top - 640){
-              $("#arrow-up").css("background","#fff");
+  // smooth navigation effect
+  $(function() {
 
-          }else{
-            $("#arrow-up").css("background","none");
-          }
-      });
+    $('a[href^="#"]').click(function(e) {
+      // prevents flickering
+      e.preventDefault();
 
-// smooth navigation effect
-$(function() {
+      var target = $(this.hash);
+      var slice = target.slice(1);
+      var anchor = $("a[name='" + slice + "']");
 
-  $('a[href^="#"]').click(function(e) {
-    // prevents flickering
-    e.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
 
-    var target = $(this.hash);
-    var slice = target.slice(1);
-    var anchor = $("a[name='" + slice + "']");
+    }); // 'a[href^="#"]'.click( function(e))
 
-    $('html, body').animate({
-      scrollTop: target.offset().top
-    }, 1000);
+  }); // $function()
 
-  }); // 'a[href^="#"]'.click( function(e))
+  }); // close out script
 
-}); // $function()
-
-}); // close out script
+// } // finish else (non mobile)
